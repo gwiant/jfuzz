8219cb2642826a325cd20ea815ac290b576ffcd6
gwiant <gwiant@me.com>
2012-11-30 22:30:26 -0800

    simplifying and optimizing -- using typed arrays, removing dom queries, using arrays instead of strings concats

--- src/jquery.jfuzz.js
+++ src/jquery.jfuzz.js
@@ -7,14 +7,19 @@
 * Requires: jQuery
 *
 */
-(function( $ ){
-	$.fn.jFuzz = function( options ) { 
+(function ( $ ){
+	$.fn.jFuzz = function ( options ) { 
 	
+		String.prototype.format = function () {
+		  var args = arguments;
+		  return this.replace(/{(\d+)}/g, function (match, number) {
+			  return typeof args[number] != 'undefined' ? (args[number] == null ? "": args[number]): match;
+		  });
+		};
+		
 		// default settings 
 		var settings = {
 			'background': 'url(assets/images/spark32x32.png)',
-			'border': false,
-			'radius': false,
 			'posX': 0,
 			'posY': 0,
 			'emitionRate': 1,
@@ -31,7 +36,7 @@
 			'shrink': 1.03,
 			'randPos': false,
 			'velocity': 10,
-			'fadeOut': false,
+			'fadeOut': true,
 			'rotate': false,
 			'skew': false,
 			'decay': .97,
@@ -39,7 +44,19 @@
 			'randomSkewRange': false,
 			'randomSizeRange': false,
 			'startOpacity': 1,
-			'startScale': 1
+			'startScale': 1,
+			'isLittleEndian': true
+		};
+		
+		var templates = {
+			cssHeight: 'height: {0}px;',
+			cssWidth: 'width: {0}px;',
+			cssBackground: 'background: url({0});',
+			cssTranslate3d: "translate3d({0}px, {1}px, 0)",
+			cssTranslate: "translate({0}px, {1}px)",
+			cssScale3d: "scale3d({0}, {1}, {2})",
+			cssScale: "scale3d({0}, {1})",
+			htmlParticle: '<div id="{0}" style="{1}"/>'
 		};
 		
 		// If options exist, lets merge them with our default settings
@@ -50,194 +67,211 @@
 		// internal settings
 		settings.emitters = [];
 		
+		// define array buffer by max particle count
+		settings.arrayBuffer = new ArrayBuffer(settings.maxParticles);
+		
+		// test for Endianness
+		/*
+		if (settings.arrayBuffer[4] === 0x0a && settings.arrayBuffer[5] === 0x0b && settings.arrayBuffer[6] === 0x0c && settings.arrayBuffer[7] === 0x0d) {
+	        settings.isLittleEndian = false;
+	    }
+	    */
+		
 		// methods
 		var methods = {
-			loop: function(i) {
-			
-				// make Particles
-				methods.makeParticles(i);
+			loop: function (thisEmitter) {
+				
+				// make Particles if needed
+				if (thisEmitter.particles.length<settings.maxParticles) {
+					methods.makeParticles(thisEmitter);	
+				}
 				
 			  	// iteratate through each particle
-				for (e=0; e<settings.emitters[i].particles.length; e++) {
+				for (p=0; p<thisEmitter.particles.length; p++) {
 				
-					var particle = settings.emitters[i].particles[e];
+					var particle = thisEmitter.particles[p];
+					
+					if (thisEmitter.scaleArray[p]>0) {
+					
+						// render particles
+						methods.render(thisEmitter, particle, p);
 						
-					// render particles
-					methods.render(i,particle);
-					
-					// update particles
-					methods.update(i,particle);
-					
-					// Keep taking the oldest particles away until we have fewer than the maximum allowed. 
-					while(settings.emitters[i].particles.length>settings.maxParticles) {
-						// remove particle
-						particle = settings.emitters[i].particles.shift(); 
-						// hide particle
-						$("#"+particle.DOMid).hide();
-						// make spare
-						settings.emitters[i].spareParticles.push(particle); 
+						// update particles
+						methods.update(thisEmitter, particle, p);
+						
+					} else {
+						methods.recycle(thisEmitter, particle);
 					}
+					
 				}
 			},
-			update: function(i,particle) { 
+			update: function (thisEmitter, particle, p) { 
+			
 				// add gravity force to the y velocity 
-				particle.velY += particle.gravity; 
+				thisEmitter.velYArray[p] += particle.gravity; 
 				
 				// shrink the particle
 				if(particle.shrink!==0) {
-					particle.scale *= particle.shrink;
-					particle.scale = ( 0 | 100 * particle.scale ) / 100;
+					thisEmitter.scaleArray[p] = ( 0 | 100 * (thisEmitter.scaleArray[p] *= particle.shrink) ) / 100;
 				}
 				
 				// add the velocity to the position
-				particle.posX += particle.velX;
+				thisEmitter.posXArray[p] += thisEmitter.velXArray[p];
+				
 				if(settings.gravity<0) {
-					particle.posY += ( particle.velY * particle.gravity );
+					thisEmitter.posYArray[p] += ( thisEmitter.velYArray[p] * particle.gravity );
 				} else {
-					particle.posY += particle.velY;
+					thisEmitter.posYArray[p] += thisEmitter.velYArray[p];
 				}
 				
-				// opacity
+				// update opacity if true with decay
 				if(settings.fadeOut===true) {
-					particle.opacity *= settings.decay;
-					particle.opacity = ( 0 | 100 * particle.opacity ) / 100;
+					thisEmitter.opacityArray[p] = ( 0 | 100 * ( thisEmitter.opacityArray[p] *= settings.decay ) ) / 100;
 				} else {
-					particle.opacity = 1;
+					thisEmitter.opacityArray[p] = 1;
 				}
 				
-				// binary shift faster than Math.round()
-				particle.posX = ( 0.5 + particle.posX >> 0 );
-				particle.posY = ( 0.5 + particle.posY >> 0 );
 			},
-			render: function(i,particle) {
-				var transStr;
+			recycle: function (thisEmitter, particle) {
+				//console.log('recycling', particle);
+				thisEmitter.scaleArray[particle.id] = settings.startScale;
+				thisEmitter.angleArray[particle.id] = ( Math.random() * Math.PI * 2 );
+				thisEmitter.speedArray[particle.id] = Math.random() * particle.randVelRange;
+				thisEmitter.velXArray[particle.id] = Math.sin(thisEmitter.angleArray[particle.id]) * thisEmitter.speedArray[particle.id]; 
+				thisEmitter.velYArray[particle.id] = Math.cos(thisEmitter.angleArray[particle.id]) * thisEmitter.speedArray[particle.id]; 
+				thisEmitter.posXArray[particle.id] = particle.randPosRange[0];
+				thisEmitter.posYArray[particle.id] = particle.randPosRange[1];
+				thisEmitter.opacityArray[p] = 1;
+			},
+			render: function (thisEmitter, particle, p) {
+			
+				var transformArray = [], transStr = "";
+				
 				if( $.browser.chrome || $.browser.safari ) {
-                    // set CSS3 transform string
-    			 	transStr = "translate3d("+particle.posX+"px, "+particle.posY+"px, 0) scale3d("+particle.scale+", "+particle.scale+", 0)"; 
+				
+                    // set position/scale
+    			 	transformArray.push(templates.cssTranslate3d.format(thisEmitter.posXArray[p], thisEmitter.posYArray[p], 0));
+    			 	transformArray.push(templates.cssScale3d.format(thisEmitter.scaleArray[p], thisEmitter.scaleArray[p], 0));
+    			 	
                 } else if( $.browser.opera ) {
-                    // set CSS3 transform string
-    			 	transStr = "translate("+particle.posX+"px, "+particle.posY+"px) scale("+particle.scale+")"; 
+                
+                    // set position/scale
+    			 	transformArray.push(templates.cssTranslate.format(thisEmitter.posXArray[p], thisEmitter.posYArray[p]));
+    			 	transformArray.push(templates.cssScale.format(thisEmitter.scaleArray[p], thisEmitter.scaleArray[p]));
+    			 	
                 } else if( $.browser.msie ) {
-                    // set CSS3 transform string
-    			 	transStr = "translate("+particle.posX+"px, "+particle.posY+"px) scale("+particle.scale+")"; 
+                
+                    // set position/scale
+    			 	transformArray.push(templates.cssTranslate.format(thisEmitter.posXArray[p], thisEmitter.posYArray[p]));
+    			 	transformArray.push(templates.cssScale.format(thisEmitter.scaleArray[p], thisEmitter.scaleArray[p]));
+    			 	
                 } else if( $.browser.mozilla ) {
-                    // set CSS3 transform string
-    			 	transStr = "translate("+particle.posX+"px, "+particle.posY+"px) scale("+particle.scale+")"; 
+                
+                    // set position/scale
+    			 	transformArray.push(templates.cssTranslate.format(thisEmitter.posXArray[p], thisEmitter.posYArray[p]));
+    			 	transformArray.push(templates.cssScale.format(thisEmitter.scaleArray[p], thisEmitter.scaleArray[p]));
+    			 	
                 }
+                
+                transStr += transformArray.join('');
                 
 			 	// update DOM element with cross-browser CSS3 transform
 				particle.$particle.css({'-webkit-transform': transStr, '-moz-transform': transStr, '-ms-transform': transStr, '-o-transform': transStr, transform: transStr, opacity: particle.opacity, display:'block'});
 			},
-			makeParticles: function(i) {
+			makeParticles: function (thisEmitter) {
+			
 				// generate particles for this(i) emitter
-				for(e=0;e<settings.emitionRate;e++) {
+				for(e=0; e<settings.emitionRate; e++) {
 				
 					// create a new particle in the stage
-					if(settings.emitters[i].spareParticles.length>0) {
-						// if one is already in the spare array, recycle it
-						particle = settings.emitters[i].spareParticles.pop(); 
-						
-						// reset particle values
-						particle.recycled = true;
-						
-					} else {
-						// otherwise make a new particle 
-						// set particle DOM id
-						var id = 'emitter'+i+'particle'+e, cssStr;
+					var cssArray = [];
+				
+					// set default particle settings
+					var particle = {
+						id: thisEmitter.particles.length,
+						DOMid: 'p'+thisEmitter.particles.length,
+						scale: 1,
+						background: settings.background,
+						recycled: false,
+						rotate: 0
+					};
 					
-						// set default particle settings
-						var particle = {
-							id: e,
-							DOMid: id,
-							scale: 1,
-							radius: settings.radius,
-							border: settings.border,
-							background: settings.background,
-							recycled: false,
-							width: settings.particleWidth,
-							height: settings.particleHeight,
-							halfWidth: ( 0.5 + ( settings.particleWidth / 2 ) >> 0 ),
-							halfHeight: ( 0.5 + ( settings.particleHeight / 2 ) >> 0 ),
-							rotate: 0,
-						};
-						
-						cssStr = 'position:absolute; width:'+particle.width+'; height: '+particle.height+';';
-						
-						// if radius
-						if(particle.radius) {
-							cssStr += ' -webkit-border-radius:'+particle.radius+';';
-						}
-						
-						// if border
-						if(particle.border) {
-							cssStr += ' border:'+particle.border+';';
-						}
-						
-						// if background
-						if(particle.background) {
-							cssStr += ' background:'+particle.background+';';
-						}
-						
-						// build particle DOM element
-						particle.$particle = $('<div id="'+particle.id+'" style="'+cssStr+'"/>');
-						
-						// inject this emitters particles
-						$("#emitter"+i+"particles").append(particle.$particle);
+					thisEmitter.widthArray[thisEmitter.particles.length] = settings.particleWidth;
+					thisEmitter.heightArray[thisEmitter.particles.length] = settings.particleHeight;
+					
+					cssArray.push('position:absolute;');
+					cssArray.push(templates.cssWidth.format(thisEmitter.widthArray[thisEmitter.particles.length]));
+					cssArray.push(templates.cssHeight.format(thisEmitter.heightArray[thisEmitter.particles.length]));
+					
+					// if background
+					if(particle.background) {
+						cssArray.push(templates.cssBackground.format(particle.background));
 					}
 					
-					// update emitter data
-					var emitterOffset = settings.emitters[i].$emitter.offset();
-					settings.emitters[i].left = ( emitterOffset.left - settings.emitters[i].parentLeft );
-					settings.emitters[i].top = ( emitterOffset.top - settings.emitters[i].parentTop );
-					//settings.emitters[i].height = height;
-					//settings.emitters[i].width = width;
-					//settings.emitters[i].halfHeight = (height / 2);
-					//settings.emitters[i].halfWidth = (width / 2);
+					// build particle DOM element
+					particle.$particle = $(templates.htmlParticle.format(particle.DOMid, cssArray.join('')));
 					
-					// update particle 
+					// inject this particle
+					thisEmitter.$emitter.append(particle.$particle);
+					
+					// update emitter data
+					var emitterOffset = thisEmitter.$emitter.offset();
+					thisEmitter.left = thisEmitter.parentLeft;
+					thisEmitter.top = thisEmitter.parentTop;
+					
+					// update particle
 					particle.opacity = settings.startOpacity;
 					particle.shrink = settings.shrink;
 					particle.drag = settings.drag;
 					particle.gravity = settings.gravity;
 					particle.scale = settings.startScale;
+					
+					// if random position is set
 					if(settings.randPos) {
 						particle.randPosRange = [
 							methods.randomRange(
-								( settings.emitters[i].left ),
-								( settings.emitters[i].left + ( settings.emitters[i].width - particle.width ) )
+								thisEmitter.left,
+								( thisEmitter.left + ( thisEmitter.width - thisEmitter.widthArray[thisEmitter.particles.length] ) )
 							),
 							methods.randomRange( 
-								( settings.emitters[i].top ),
-								( settings.emitters[i].top + ( settings.emitters[i].height - particle.height ) )
+								thisEmitter.top,
+								( thisEmitter.top + ( thisEmitter.height - thisEmitter.heightArray[thisEmitter.particles.length] ) )
 							)
 						];
 					} else {
-						particle.randPosRange = [settings.emitters[i].left,settings.emitters[i].top];
+						particle.randPosRange = [thisEmitter.left,thisEmitter.top];
 					}
+					
 					if(settings.velocityRange) {
 						particle.randVelRange = methods.randomRange(settings.velocityRange[0],settings.velocityRange[1]);
 					}
-					particle.posX = particle.randPosRange[0];
-					particle.posY = particle.randPosRange[1];
 					
 					// ensure circular distribution of particles, for a more natural appearance
-					particle.angle = ( Math.random() * Math.PI * 2 );
-					particle.speed = Math.random() * particle.randVelRange;
-					particle.velX = Math.sin(particle.angle) * particle.speed; 
-					particle.velY = Math.cos(particle.angle) * particle.speed; 
+					thisEmitter.angleArray[thisEmitter.particles.length] = ( Math.random() * Math.PI * 2 );
+					thisEmitter.speedArray[thisEmitter.particles.length] = Math.random() * particle.randVelRange;
+					thisEmitter.velXArray[thisEmitter.particles.length] = Math.sin(thisEmitter.angleArray[thisEmitter.particles.length]) * thisEmitter.speedArray[thisEmitter.particles.length]; 
+					thisEmitter.velYArray[thisEmitter.particles.length] = Math.cos(thisEmitter.angleArray[thisEmitter.particles.length]) * thisEmitter.speedArray[thisEmitter.particles.length]; 
+					thisEmitter.posXArray[thisEmitter.particles.length] = particle.randPosRange[0];
+					thisEmitter.posYArray[thisEmitter.particles.length] = particle.randPosRange[1];
+					thisEmitter.scaleArray[thisEmitter.particles.length] = particle.scale;
 					
 					// add particle to this emitters particle collection
-					settings.emitters[i].particles.push(particle);
+					thisEmitter.particles.push(particle);
 					
 				}
 			},
-			randomRange: function(min, max) {
+			randomRange: function (min, max) {
+			
 				// return random range
 				return ((Math.random()*(max-min)) + min); 
+				
 			},
-			init : function(i) { 
+			init : function (i) { 
 			
+				var thisEmitter = settings.emitters[i];
+				
 				settings.userAgent = navigator.userAgent.toLowerCase();
+				
                 // Which browser?
                 $.browser = {
                 	version: (settings.userAgent.match( /.+(?:rv|it|ra|ie|me)[\/: ]([\d.]+)/ ) || [])[1],
@@ -249,21 +283,45 @@
                 };
                 
 				// inject current(i) emitters particle container
-				settings.emitters[i].$emitter.after('<div id="emitter'+i+'particles"></div>');
+				thisEmitter.$emitter.after('<div id="emitter'+i+'particles"></div>');
 				
-				settings.emitters[i].loop = setInterval(function(){ methods.loop(i) }, 1000 / settings.frameRate);
+				thisEmitter.loop = setInterval(function (){ methods.loop(thisEmitter) }, 1000 / settings.frameRate);
+				
 			}
 		};
 		
-		return this.each(function( i, item ) {
+		return this.each(function ( i, item ) {
+			
 			// get emitter object
 			var emitterOffset = $(item).offset(), width = $(item).width(), height = $(item).height();
 			var parentOffset = $(item).parent().offset();
-			var emitter = { $emitter: $("#"+$(item).attr('id')), particles: [], width: width, height: height, halfHeight: (height / 2), halfWidth: (width / 2), top: emitterOffset.top, left: emitterOffset.left, parentLeft: parentOffset.left, parentTop: parentOffset.top, spareParticles: [] };
+			var emitter = { 
+				$emitter: $("#"+$(item).attr('id')), 
+				particles: [], 
+				posXArray: new Float32Array(settings.arrayBuffer.byteLength), 
+				posYArray: new Float32Array(settings.arrayBuffer.byteLength), 
+				scaleArray: new Float32Array(settings.arrayBuffer.byteLength), 
+				opacityArray: new Float32Array(settings.arrayBuffer.byteLength),
+				heightArray: new Float32Array(settings.arrayBuffer.byteLength),  
+				widthArray: new Float32Array(settings.arrayBuffer.byteLength), 
+				angleArray: new Float32Array(settings.arrayBuffer.byteLength), 
+				speedArray: new Float32Array(settings.arrayBuffer.byteLength), 
+				velXArray: new Float32Array(settings.arrayBuffer.byteLength), 
+				velYArray: new Float32Array(settings.arrayBuffer.byteLength), 
+				width: width, 
+				height: height,  
+				top: emitterOffset.top, 
+				left: emitterOffset.left, 
+				parentLeft: parentOffset.left, 
+				parentTop: parentOffset.top
+			};
+			
 			// add emitter to emitters container
 			settings.emitters.push(emitter);
+			
 			// initialize this emitter
 			methods.init(i);
+			
 		});
 	};
 })( jQuery );
